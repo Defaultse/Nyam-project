7f1bb8cc7bb3a45f31b6ce8d7c047648eaeb1062
Defaultse <45491587+Defaultse@users.noreply.github.com>
2020-11-20 12:41:17 +0600

    Removed ugly css. Added new functionality to Recipes.
    Need bootstrap instead of CSS.

--- react-project/src/Recipes/Recipes.tsx
+++ react-project/src/Recipes/Recipes.tsx
@@ -9,23 +9,32 @@ interface Props {
 export default function Recipes({}: Props): ReactElement { 
     const APP_ID = "bcf72ecd";
     const APP_KEY = "3eba030c527845f34fe665899441bd71";
-    const [recipes, setRecipes] = useState<any[]>([]);
 
-    useEffect(()=>{
+    const [recipes, setRecipes] = useState<any[]>([]);
+    const [search, setSearch] = useState("");
+    const [query, setQuery] = useState("chicken");
+
+    useEffect(()=>{ 
        getRecepies();
-    }, [])
+    }, [query])
 
     const getRecepies = async() => {
-        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
         const data = await response.json();
         setRecipes(data.hits);
         console.log(data.hits);
     }
 
+    const getSearch = (e:any) => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch('');
+    }
+
     return (
         <div>
-            <form>
-                <input type="text"/>
+            <form onSubmit={getSearch}>
+                <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)}/>
                 <button type="submit">Search</button>
             </form>
             {recipes.map(recipe => (
